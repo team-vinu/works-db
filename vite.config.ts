ba9257c80a9686a98b8e086101c7b4440749d4b6
wadeyusaku <wadeyusaku@gmail.com>
2022-02-24 12:24:05 +0900

    change to react from vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vanillaExtractPlugin()]
+  plugins: [react(), vanillaExtractPlugin()]
 })
