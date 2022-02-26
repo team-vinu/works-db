c057a649b6f60f31b953e199c3e4180b26385f22
Satsuki Akiba <anosatsuk124@gmail.com>
2022-02-26 15:40:30 +0000

    migrate to mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,74 +1,58 @@
 import React from "react";
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Code,
+    Text,
+    Link,
+    HStack,
+    Box,
+    Center,
+    Heading,
+    Button,
+    Input,
+    Container,
+    Icon,
+    Switch,
+    useColorMode,
+    NativeBaseProvider,
+    extendTheme,
+    VStack,
+    Code,
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+import { UserOutlined } from "@ant-design/icons";
+//import NativeBaseIcon from "./components/NativeBaseIcon";
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
-type MyThemeType = typeof theme;
-declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
-}
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
-}
+    return (
+        <NativeBaseProvider>
+            <Box m="4">
+                <Box mb="4">
+                    <Heading py="2">Works DB</Heading>
+                    <Box display="flex">
+                        <Input
+                            placeholder="キーワードを入力してください"
+                            mr="2"
+                        />
+                        <Button>検索</Button>
+                    </Box>
+                </Box>
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+                <Box mb="4">
+                    <Heading size="sm">ピックアップ</Heading>
+                    <HStack overflow="scroll">
+                        {(() => {
+                            const items = [];
+                            for (let i = 0; i < 10; i++) {
+                                items.push(
+                                    <Container centerContent key={i} p="0">
+                                        {/* <Icon as={UserOutlined} /> */}
+                                        <Text>creator</Text>
+                                    </Container>
+                                );
+                            }
+                            return items;
+                        })()}
+                    </HStack>
+                </Box>
+            </Box>
+        </NativeBaseProvider>
+    );
 }
