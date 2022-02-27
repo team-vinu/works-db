ffd0aea5d4c3a942177636b20f82570176a71863
wadeyusaku <wadeyusaku@gmail.com>
2022-02-27 19:33:47 +0900

    add react router dom

---
+++ web/src/tsx/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from './views/Home'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
