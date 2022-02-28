49ceddf0c222133003354c155bfa192e42054cef
wadeyusaku <wadeyusaku@gmail.com>
2022-02-28 12:14:34 +0900

    define brand colors

--- web/src/ts/theme.ts
+++ web/src/ts/theme.ts
@@ -1,7 +1,15 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from '@chakra-ui/react';
 
-const theme = extendTheme({
-    
-});
+/* Object */
+const brandColors: Object = {
+    'Azure X 11 Web Color': 'EBFFFD',
+    Celeste: 'BCF1EB',
+    'Middle Blue Green': '8DE3DA',
+    'Medium Turquoise': '5ED4C8',
+    'Tiffany Blue': '2FC6B7',
+    Keppel: '00B8A5',
+};
+
+const theme = extendTheme({});
 
 export default theme;
