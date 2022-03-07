44d96c795b3693451cae7a3ca671b09154ee98d3
Satsuki Akiba <anosatsuk124@gmail.com>
2022-03-08 08:40:08 +0900

    add docker

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
             .service(router::post_music)
             .service(router::get_music)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await;
 }
