225c3d5485e829977490982d66c784efaa8651d8
Satsuki Akiba <anosatsuk124@gmail.com>
2022-04-01 06:19:09 +0000

    fix about open admin-sdk.json

---
+++ docs/API/env.md
@@ -0,0 +1,5 @@
+# environment variables
+
+## Server
+- DATABASE_URL ( to connect to the database. at router.rs )
+- ADMIN_SDK_PATH ( to configure firebase. at config.rs )
--- server/src/firebase/config.rs
+++ server/src/firebase/config.rs
@@ -1,3 +1,6 @@
+use dotenv::dotenv;
+use std::env;
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct FirebaseConfig {
     pub project_id: String,
@@ -9,7 +12,10 @@ pub struct FirebaseConfig {
 
 impl FireabaseConfig {
     pub fn new() -> FirebaseConfig {
+        dotenv().ok;
+
         let file = File::open("").unwrap(); //TODO: Firebase-admindsk.jsonのパス (dotenvを使う。環境変数のdoc)
+        let path = env::var("ADMIN_SDK_PATH").expect("ADMIN_SDK_PATH must be set");
         let reader = BufReader::new(file);
         let config: FireabaseConfig = serde_json::from_reader(reader).unwrap();
         return config;
